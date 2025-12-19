81e0b6671b23f235cc434758a0fc5801a29adb61
Vishwanathms <vishwanath.murthy@gmail.com>
2025-12-19 14:53:31 +0530

    added jenkinsfile for selenium

---
+++ src/test/java/com/dev3l/hello_world/SampleTest.java
@@ -0,0 +1,12 @@
+package com.dev3l.hello_world;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class SampleTest {
+
+    @Test
+    public void sanityTest() {
+        assertTrue(true);
+    }
+}
